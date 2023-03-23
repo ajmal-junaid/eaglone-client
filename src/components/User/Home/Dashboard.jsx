3b9dbc20a600ebb039093bbf6eb94b5c9257ae8c
Ajmal junaid <ajmal6201@gmail.com>
2023-03-23 11:46:05 +0530

    fix

--- src/components/User/Home/Dashboard.jsx
+++ src/components/User/Home/Dashboard.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { baseUrl } from "../../../utils/constants";
-import Loading from "../../Common/Loading";
 import sweetAlert from "../../Common/SweetAlert";
 
 function Dashboard() {
@@ -51,7 +50,7 @@ function Dashboard() {
   return (
     <>
       <div className="container mx-auto px-6 py-24 flex flex-col lg:flex-row items-start lg:items-center justify-between">
-            <Loading />
+            
         <div className="w-full">
           <div>
           </div>

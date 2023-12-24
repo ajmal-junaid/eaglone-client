dc0922d5b851dc942a724e8f652d2bf2b27503c6
Ajmal junaid <94582330+ajmal-junaid@users.noreply.github.com>
2023-12-24 12:33:32 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 // export const baseUrl = "https://www.eaglone.online/api/v1/"
 // export const adminBaseUrl = "https://www.eaglone.online/api/v1/admin"
-export const baseUrl = "http://localhost:4000/api/v1/"
-export const adminBaseUrl = "http://localhost:4000/api/v1/admin"
+//export const baseUrl = "http://localhost:4000/api/v1/"
+//export const adminBaseUrl = "http://localhost:4000/api/v1/admin"
+
+export const baseUrl = "https://eaglone-api.onrender.com/api/v1/"
+export const adminBaseUrl = "https://eaglone-api.onrender.com/api/v1/admin"
 
 
